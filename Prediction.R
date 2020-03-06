e42319612d7762dce8984057bde8ccf0f8ddeb01
Varun Mishra <varun11mishra@gmail.com>
2020-03-06 17:13:43 +0530

    Update Prediction.R

--- Prediction.R
+++ Prediction.R
@@ -18,9 +18,20 @@ StudentData$programming <- (round((StudentData$Y1S5 + StudentData$Y2S5 +
 LinearModel1 <- lm(StudentData$numerica~StudentData$programming, 
                               data = StudentData)
 LinearModel1
-
-
 PredictedData$Numerical[1:999] <- (((64.5129*StudentData$programming[1:999])
                                     + 0.007668)/100)
-PredictedData$Numerical
+
+LinearModel2 <- lm(StudentData$theoretical~StudentData$programming,
+                   data = StudentData)
+LinearModel2
+PredictedData$Theoretical[1:999] <- (((61.75212*StudentData$programming[1:9990])
+                                    + 0.04428)/100)
+
+LinearModel3 <- lm(StudentData$programming~(StudentData$numerica+StudentData$theoretical),
+                   data = StudentData)
+LinearModel3
+PredictedData$Programming[1:999] <- (((62.108*StudentData$theoretical[1:999])
+                                      + 0.042)/100)
+PredictedData$Programming
+
 
